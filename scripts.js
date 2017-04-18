16a5c45223c339930a4ce9378fdf1c29d078fe7d
Andrew Hastings <droo.hastings@gmail.com>
2017-04-18 10:38:31 -0500

    init commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log('sourcing works!');
+//
+// <img src='https://github.com/neotamoe.png' alt='Profile image of Neota'>
+// <img src='https://github.com/Mong005e.png' alt='Profile image of Droo'>
+// <img src='https://github.com/karabayse.png' alt='Profile image of Kara'>
+// <img src='https://github.com/benjlaud1.png' alt='Profile image of Ben'>
+
+$(document).ready(onReady);
+
+function onReady (){
+
+console.log('jquery works!!');
+
+
+$('.container')
+}
